69c9314c555e47794c10580e840fa879da1c9008
James Skemp <strivinglife@gmail.com>
2015-07-18 21:12:26 -0500

    Standard updates for widths and heights, title, and Android needs.

--- desktop/src/com/jamesrskemp/tappingdemo/desktop/DesktopLauncher.java
+++ desktop/src/com/jamesrskemp/tappingdemo/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.jamesrskemp.tappingdemo.TappingDemoGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "libGDX Tapping Demo";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new TappingDemoGame(), config);
 	}
 }
