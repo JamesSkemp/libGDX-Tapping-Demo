8e7f0ef5bbedb9ff59d8d273b3ad01241917d475
James Skemp <strivinglife@gmail.com>
2015-07-18 21:09:37 -0500

    Initial commit of new project.

---
+++ desktop/src/com/jamesrskemp/tappingdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jamesrskemp.tappingdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jamesrskemp.tappingdemo.TappingDemoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TappingDemoGame(), config);
+	}
+}
